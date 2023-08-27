38c437ae66db14f1488f8ee9fd9044b0aeea854d
TETSU-A7240 <134199699+TETSU-A7240@users.noreply.github.com>
2023-08-27 16:59:21 +0900

    itemsのマイグレーションファイルの修正
    対象項目にidの付与（_idのつけ忘れ）

--- db/migrate/20230825140919_create_items.rb
+++ db/migrate/20230825140919_create_items.rb
@@ -4,11 +4,11 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :item_name, null: false
       t.integer :price, null: false
       t.text :description, null: false
-      t.integer :condition, null: false
-      t.integer :category, null: false
-      t.integer :postage, null: false
-      t.integer :region, null: false
-      t.integer :shipping_date, null: false
+      t.integer :condition_id, null: false
+      t.integer :category_id, null: false
+      t.integer :postage_id, null: false
+      t.integer :region_id, null: false
+      t.integer :shipping_date_id, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_27_050839) do
+ActiveRecord::Schema.define(version: 2023_08_27_070533) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,15 +33,20 @@ ActiveRecord::Schema.define(version: 2023_08_27_050839) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.integer "price", null: false
     t.text "description", null: false
-    t.integer "condition", null: false
-    t.integer "category", null: false
-    t.integer "postage", null: false
-    t.integer "region", null: false
-    t.integer "shipping_date", null: false
+    t.integer "condition_id", null: false
+    t.integer "category_id", null: false
+    t.integer "postage_id", null: false
+    t.integer "region_id", null: false
+    t.integer "shipping_date_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
