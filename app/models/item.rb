cfa9ff4102eaf2c05dd2b9fca4d7a08543765518
TETSU-A7240 <134199699+TETSU-A7240@users.noreply.github.com>
2023-08-27 20:02:52 +0900

    各プルダウンメニューとのアソシエーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :shipping_date
 
 end
