14e303a20673ded802ed4ad075a3ebf748fc00f1
TETSU-A7240 <134199699+TETSU-A7240@users.noreply.github.com>
2023-08-27 20:45:19 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,15 @@ class Item < ApplicationRecord
   belongs_to :region
   belongs_to :shipping_date
 
+  validates :image, :item_name, :description, :price,  presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :price, numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999,message: 'must be between ¥300 and ¥9,999,999' }
+  validates :price, format: { with: /\A\d+\z/, message: 'must be a valid number' }
+
 end
