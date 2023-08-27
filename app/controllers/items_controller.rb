5006aa5a8635e085d140e7bb226741e9248f93df
TETSU-A7240 <134199699+TETSU-A7240@users.noreply.github.com>
2023-08-27 17:48:08 +0900

    itemコントローラーとルーティング(resouces)の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,24 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @Items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-  end
-  
+  def item_params
+    params.require(:item).permit(:item_name, :price, :description, :condition_id, :category_id, :postage_id, :region_id, :shipping_date_id, :user)
+
 end
