b559464c722cd1d5813ab9f6f8d21104706c7d7f
TETSU-A7240 <134199699+TETSU-A7240@users.noreply.github.com>
2023-09-02 04:56:01 +0900

    バリデーションを分解

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,10 @@ class Item < ApplicationRecord
   belongs_to :region
   belongs_to :shipping_date
 
-  validates :image, :item_name, :description,price, presence: true
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
