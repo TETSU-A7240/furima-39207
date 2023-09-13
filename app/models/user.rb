c78a40a92cb2dc7b9261c299a4fcbe326ad1b011
TETSU-A7240 <134199699+TETSU-A7240@users.noreply.github.com>
2023-09-13 20:01:18 +0900

    レビュー修正3回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_one :orders
+  has_one :order
 
   validates :nickname, presence: true
   validates :first_name, presence: true
