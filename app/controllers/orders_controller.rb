31943e94759d467e04ebfa18dadfe84e1b15964c
TETSU-A7240 <134199699+TETSU-A7240@users.noreply.github.com>
2023-09-13 14:22:28 +0900

    環境変数の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,7 +24,7 @@ class OrdersController < ApplicationController
   end
   
   def pay_item
-    Payjp.api_key = "sk_test_1b285fb3a7428b06ac2f4446" 
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item[:price],
       card: order_form_params[:token],
